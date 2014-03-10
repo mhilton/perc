1a9129b535f96354a5df8927a002d01c483b6f6a
Martin Hilton <martin.hilton@zepler.net>
2014-03-10 21:53:12 +0000

    Added a time remaining estimate

--- perc.go
+++ perc.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -28,21 +29,41 @@ func main() {
 		}
 	} else {
 		var sum uint64
+		var prev uint64
+		ra := NewRunningAverage(5)
+
 		for n := range c {
 			sum += uint64(n)
 			perc := (sum * 100) / size
-			fmt.Fprintf(os.Stderr, "\r%3d%%", perc)
+			for ; prev < perc; prev++ {
+				ra.Sample()
+			}
+
+			if perc == 0 {
+				fmt.Fprintf(os.Stderr, "\r  0%% estimated time remaining --:--:--")
+				continue
+			}
+
+			remaining := time.Duration(100-perc)*ra.Average() - ra.Since()
+			hours := remaining / time.Hour
+			remaining -= hours * time.Hour
+			mins := remaining / time.Minute
+			remaining -= mins * time.Minute
+			secs := remaining / time.Second
+
+			fmt.Fprintf(os.Stderr, "\r%3d%% estimated time remaining %02d:%02d:%02d", perc, hours, mins, secs)
 		}
+
 		fmt.Fprintln(os.Stderr, "")
 	}
 }
 
-// copy copies bytes from in to out, updating c with number of bytes copied 
-// in each iteration. 
+// copy copies bytes from in to out, updating c with number of bytes copied
+// in each iteration.
 func copy(in io.Reader, out io.Writer, c chan int) {
 	defer close(c)
 
-	b := make([]byte, 8 * 1024)
+	b := make([]byte, 8*1024)
 
 	for {
 		n, err := in.Read(b)
@@ -55,7 +76,7 @@ func copy(in io.Reader, out io.Writer, c chan int) {
 			}
 		}
 
-		_, err = out.Write(b[:n]) 
+		_, err = out.Write(b[:n])
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Output error: %s\n", err)
 			os.Exit(1)
@@ -63,4 +84,47 @@ func copy(in io.Reader, out io.Writer, c chan int) {
 
 		c <- n
 	}
+}
+
+// A RunningAverage maintains a running average of times between samples
+type RunningAverage struct {
+	samples []time.Duration
+	count   int
+	last    time.Time
+}
+
+// NewRunningAverage makes a new RunningAverage with size sample points
+func NewRunningAverage(size int) *RunningAverage {
+	return &RunningAverage{make([]time.Duration, size), 0, time.Now()}
+}
+
+// Sample saves a new sample in the RunningAverage
+func (r *RunningAverage) Sample() {
+	next := time.Now()
+	r.samples[r.count%len(r.samples)] = next.Sub(r.last)
+	r.count++
+	r.last = next
+}
+
+// Average works out the current running average
+func (r *RunningAverage) Average() time.Duration {
+	if r.count == 0 {
+		return time.Duration(0)
+	}
+
+	var sum time.Duration
+	for _, t := range r.samples {
+		sum += t
+	}
+
+	if r.count < len(r.samples) {
+		return sum / time.Duration(r.count)
+	} else {
+		return sum / time.Duration(len(r.samples))
+	}
+}
+
+// Since returns the time since the last sample
+func (r *RunningAverage) Since() time.Duration {
+	return time.Since(r.last)
 }
